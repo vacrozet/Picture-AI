6173624b1dacf73ec53e161032a6c6b38cfaf300
vacrozet <vacrozet@student.42.fr>
2018-01-29 17:17:44 -0300

    mail

--- app/controller/user/signup.js
+++ app/controller/user/signup.js
@@ -24,12 +24,50 @@ module.exports = (req, res) => {
             console.log('envoi du mail -->')
             sendmail({
               from: 'soyuz.digital@soyuz.digital',
-              to: 'valentin@soyuz.digital',
+              to: req.body.mail,
               replyTo: 'valentin@soyuz.digital.com',
-              subject: 'Nouveau mot de passe Picture_AI',
-              html: `Bonjour,<br />
-                    Votre Identifiant Soyuz Digital Pictue AI est ${req.body.mail} <br />
-                    Votre mot de passe par default est '${pass}'`
+              subject: 'Bienvenue sur Picture AI @ Soyuz Digital',
+              html:
+              `<html>
+              <head>
+                  <meta http-equiv='content-type' content='text/html; charset=utf-8'>
+                  <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'>
+              </head>
+              <body leftmargin='0px' topmargin='0px' marginwidth='0px' marginheight='0px'>
+                <table bgcolor='#262931' valign='top' height='100%' width='100%' cellpadding='0' cellspacing='0' border='0'>
+                  <tbody>
+                  <tr>
+                    <td>
+                      <div>
+                          <table bgcolor='#262931' align='center' height='100%' width='100%' cellpadding='0' cellspacing='0' border='0'>
+                            <tbody>
+                              <tr><td height='60px'></td></tr>
+                              <tr><td align='center'><a href='http://localhost:3000/'><img class='name' src='https://soyuz.digital/wp-content/themes/soyouz2016/images/logo-soyuz.png' alt='SoyuzDigital'></a></td></tr>
+                              <tr height='30px;'><td></td></tr>
+                              <tr>
+                                <td align='center' style='font-size: 22px; text-align: center; color: white; font-family: brandon-grotesque, sans-serif;'>Bienvenue sur Picture AI</td>
+                              </tr>
+                              <tr height='20px;'><td></td></tr>
+                              <tr><td align='center' style='font-size: 18px; text-align: center; color: white; font-family: brandon-grotesque, sans-serif;'> Un compte a été créé pour vous</td></tr>
+                              <tr height='20px;'><td></td></tr>
+                              <tr><td align='center' style='font-size: 18px; text-align: center; color: white; font-family: brandon-grotesque, sans-serif;'>Identifiant:</td></tr>
+                              <tr height='5px;'><td></td></tr>
+                              <tr><td align='center' style='font-size: 16px; text-align: center; color: #ffcc00; font-family: brandon-grotesque, sans-serif;'>${req.body.mail}</td></tr>
+                              <tr height='10px;'><td></td></tr>
+                              <tr><td align='center' style='font-size: 18px; text-align: center; color: white; font-family: brandon-grotesque, sans-serif;'>Password:</td></tr>
+                              <tr><td align='center' style='font-size: 16px; text-align: center; color: #ffcc00; font-family: brandon-grotesque, sans-serif;'>${pass}</td></tr>
+                              <tr height='30px;'><td></td></tr>
+                              <tr><td align='center' style='text-decoration: none; font-size: 20px; text-align: center; color: #ffcc00; font-family: brandon-grotesque, sans-serif; border-spacing: 10px;'><a href='http://localhost:3000/' style='text-decoration: none; color: #ffcc00; font-size: 30px;'>Picture AI</a></td></tr>
+                              <tr height='60px;'><td></td></tr>
+                            </tbody>
+                          </table>
+                      </div>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </body>
+              </html>`
             }, (err, reply) => {
               if (err) console.log(err)
               if (reply) {
