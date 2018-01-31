463e147707c6bcd132359f1078d540d8ec96aab2
vacrozet <vacrozet@student.42.fr>
2018-01-31 14:57:47 -0300

    Command

--- src/routes/Signin.js
+++ src/routes/Signin.js
@@ -8,51 +8,67 @@ const FormItem = Form.Item
 class Signin extends Component {
   constructor (props) {
     super(props)
+    this._mount = true
     this.state = {
       mail: '',
       password: '',
       loading: false
     }
+    this.handleError = this.handleError.bind(this)
+    this.handlekeyPress = this.handlekeyPress.bind(this)
   }
 
   componentWillMount () {
+    this._mount = true
     // console.log(this.props.location.pathname)
+  }
+  componentWillUnmount () {
+    this._mount = false
   }
 
   handleChange (evt) {
     this.setState({[evt.target.name]: evt.target.value})
   }
-
-  handleSendData () {
-    if (this.state.mail !== '' && this.state.password !== '') {
-      this.setState({loading: true})
-      local().get('/user/signin', {
-        params: {
-          mail: this.state.mail,
-          password: this.state.password
-        }
-      }).then((res) => {
-        if (res.data.success === true) {
-          this.props.handleConnexion(true, res.data.superUser)
-          global.localStorage.setItem('token', res.data.token)
-          if (res.data.lock === true) {
-            global.localStorage.setItem('lock', true)
-            this.props.history.push('/changepass')
-          } else {
-            global.localStorage.setItem('lock', false)
-            this.props.history.push('/home')
+  handleError (div) {
+    var effet = document.getElementById(div)
+    effet.style.transition = '0.6s'
+    effet.style.border = 'solid 2px #FA0000'
+    setTimeout(() => {
+      if (this._mount === true) {
+        var effet = document.getElementById(div)
+        effet.style.transition = '0.6s'
+        effet.style.border = 'none'
+      }
+    }, 5000)
+  }
+  handlekeyPress (evt) {
+    if (evt.key === 'Enter' || evt.target.value === 'connexion') {
+      if (this.state.mail !== '' && this.state.password !== '') {
+        this.setState({loading: true})
+        local().get('/user/signin', {
+          params: {
+            mail: this.state.mail,
+            password: this.state.password
           }
-        }
-      }).catch((err) => {
-        this.setState({
-          loading: false
+        }).then((res) => {
+          if (res.data.success === true) {
+            this.props.handleConnexion(true, res.data.superUser)
+            global.localStorage.setItem('token', res.data.token)
+            if (res.data.lock === true) {
+              global.localStorage.setItem('lock', true)
+              this.props.history.push('/changepass')
+            } else {
+              global.localStorage.setItem('lock', false)
+              this.props.history.push('/home')
+            }
+          }
+        }).catch((err) => {
+          this.setState({
+            loading: false
+          })
+          if (err.response.data.success === false) this.handleError(err.response.data.div)
         })
-        if (err.response.data.success === false) {
-          var effet = document.getElementById(err.response.data.div)
-          effet.style.transition = '0.6s'
-          effet.style.border = 'solid 2px #FA0000'
-        }
-      })
+      }
     }
   }
 
@@ -63,12 +79,12 @@ class Signin extends Component {
           <center className='title'>Welcome !</center><br />
           <Form>
             <FormItem>
-              <Input id='mail' name='mail' value={this.state.mail} onChange={this.handleChange.bind(this)} prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Adresse Mail' />
-              <Input id='password' name='password' value={this.state.password} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Password' />
+              <Input id='mail' name='mail' value={this.state.mail} onChange={this.handleChange.bind(this)} onKeyPress={this.handlekeyPress} prefix={<Icon type='user' style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder='Adresse Mail' />
+              <Input id='password' name='password' value={this.state.password} onChange={this.handleChange.bind(this)} onKeyPress={this.handlekeyPress} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Password' />
               <center>
                 <Checkbox className='text'>Se souvenir de moi</Checkbox><br />
                 <Link className='text link' to='/signin/reset'>Mot de passe oublié ?</Link><br />
-                <Button type='primary' className='button' onClick={this.handleSendData.bind(this)} loading={this.state.loading}>
+                <Button type='primary' value='connexion' className='button' onKeyPress={this.handlekeyPress} onClick={this.handlekeyPress} loading={this.state.loading}>
                   Connexion
                 </Button>
               </center>
--- src/routes/changepass.js
+++ src/routes/changepass.js
@@ -7,43 +7,75 @@ const FormItem = Form.Item
 class ChangePass extends Component {
   constructor (props) {
     super(props)
+    this._mount = true
     this.state = {
       oldPass: '',
       newPass: '',
       newPass1: ''
     }
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleError = this.handleError.bind(this)
   }
 
   componentWillMount () {
+    this._mount = true
     const lock = global.localStorage.getItem('lock')
     if (lock === false) {
       this.props.history.push('/home')
     }
   }
+  componentWillUnmount () {
+    this._mount = false
+  }
 
   handleChange (evt) {
+    console.log(evt.target.name)
     this.setState({[evt.target.name]: evt.target.value})
   }
-  handleSendData () {
-    if (this.state.oldPass !== undefined && this.state.newPass !== undefined && this.state.newPass1 === this.state.newPass) {
-      local().put('/user/passwordlost', {
-        oldPass: this.state.oldPass,
-        newPass: this.state.newPass,
-        newPass1: this.state.newPass1
-      }).then((res) => {
-        if (res.data.success === true) {
-          global.localStorage.removeItem('lock')
-          this.props.history.push('/home')
-        } else {
-          this.setState({
-            oldPass: '',
-            newPass: '',
-            newPass1: ''
-          })
+
+  handleError (div) {
+    var effet = document.getElementsByClassName(div)
+    for (let index = 0; index < effet.length; index++) {
+      effet[index].style.transition = '0.6s'
+      effet[index].style.border = 'solid 2px #FA0000'
+    }
+    setTimeout(() => {
+      if (this._mount === true) {
+        for (let index = 0; index < effet.length; index++) {
+          effet[index].style.transition = '0.6s'
+          effet[index].style.border = 'none'
         }
-      }).catch((err) => {
-        console.log(err)
-      })
+      }
+    }, 5000)
+  }
+
+  handleKeyPress (evt) {
+    console.log('je passe')
+    if (evt.key === 'Enter' || evt.target.value === 'changePass') {
+      if (this.state.oldPass !== '' && this.state.newPass !== '' &&
+      this.state.newPass1 !== '' && this.state.newPass === this.state.newPass1 &&
+      this.state.oldPass !== this.state.newPass) {
+        local().put('/user/passwordlost', {
+          oldPass: this.state.oldPass,
+          newPass: this.state.newPass,
+          newPass1: this.state.newPass1
+        }).then((res) => {
+          if (res.data.success === true) {
+            global.localStorage.removeItem('lock')
+            this.props.history.push('/home')
+          } else {
+            this.setState({
+              oldPass: '',
+              newPass: '',
+              newPass1: ''
+            })
+          }
+        }).catch((err) => {
+          console.log(err)
+        })
+      } else if (this.state.newPass !== this.state.newPass1) {
+        this.handleError('newPass')
+      } else this.handleError('Pass')
     }
   }
   render () {
@@ -53,11 +85,11 @@ class ChangePass extends Component {
           <center className='title'>Reset Password !</center><br />
           <Form>
             <FormItem>
-              <Input name='oldPass' value={this.state.oldPass} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Mot de passe Provisoire' />
-              <Input name='newPass' value={this.state.newPass} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Nouveau Mot de Passe' />
-              <Input name='newPass1' value={this.state.newPass1} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Nouveau Mot de Passe' />
+              <Input name='oldPass' className='Pass' value={this.state.oldPass} onKeyPress={this.handleKeyPress} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Mot de passe Provisoire' />
+              <Input className='newPass Pass' name='newPass' value={this.state.newPass} onKeyPress={this.handleKeyPress} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Nouveau Mot de Passe' />
+              <Input className='newPass Pass' name='newPass1' value={this.state.newPass1} onKeyPress={this.handleKeyPress} onChange={this.handleChange.bind(this)} prefix={<Icon type='lock' style={{ color: 'rgba(0,0,0,.25)' }} />} type='password' placeholder='Nouveau Mot de Passe' />
               <center>
-                <Button type='primary' className='button' onClick={this.handleSendData.bind(this)}>
+                <Button value='changePass' type='primary' className='button' onKeyPress={this.handleKeyPress} onClick={this.handleKeyPress}>
                   Modifier
                 </Button>
               </center>
